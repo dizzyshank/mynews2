467826a0e380eb9b6266e066daa7f36c8e0994fa
homma reo <uvuw092544@gmail.com>
2023-01-02 07:07:12 +0000

    profile/edit.blade.php 編集

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,13 +1,12 @@
 @extends('layouts.admin')
-@section('title', 'My プロフィール')
+@section('title', 'プロフィールの編集')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
-                <form action="{{ route('admin.profile.edit') }}" method="post" enctype="multipart/form-data">
-
+                <h2>プロフィール編集</h2>
+                <form action="{{ route('admin.news.update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -16,39 +15,36 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">氏名(name)</label>
+                        <label class="col-md-2" for="name">氏名</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">性別(gender)</label>
+                        <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">趣味(hobby)</label>
+                        <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                        <label class="col-md-2" for="introduction">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
                         </div>
                     </div>
-                    <?php /*
                     <div class="form-group row">
-                        <label class="col-md-2">画像</label>
                         <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
+                            <input type="hidden" name="id" value="{{ $news_form->id }}">
+                            @csrf
+                            <input type="submit" class="btn btn-primary" value="更新">
                         </div>
                     </div>
-                    */ ?>
-                    @csrf
-                    <input type="submit" class="btn btn-primary" value="更新">
                 </form>
             </div>
         </div>
