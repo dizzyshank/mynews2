ed3bbdefaba94072277f43def48d78cc0eb576df
homma reo <uvuw092544@gmail.com>
2023-01-02 05:51:08 +0000

    NewsController.php 画像変更時の処理を追加

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -79,6 +79,19 @@ class NewsController extends Controller
         $news = News::find($request->id);
         // 送信されてきたフォームデータを格納する
         $news_form = $request->all();
+        
+        //画像変更時エラーにならないように条件分岐
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+        
+        unset($news_form['image']);
+        unset($news_form['remove']);
         unset($news_form['_token']);
 
         // 該当するデータを上書きして保存する
